ea61b00f2717fe4f7a56cca105545d73bc73fa2c
vihuro <vitorhugorodrigues93@gmail.com>
2023-03-29 18:08:47 -0300

    criado tela de login

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0942a82af7119495"; }
+/******/ 		__webpack_require__.h = function() { return "38daa0dbd3378cd7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,69 @@
 import style from "./index.module.css"
-import{parseCookies}from "nookies"
+import { parseCookies } from "nookies"
+import { useState } from "react";
+import Button from "../components/ui/button/ButtonUi"
 
-function Home(){
+function Home() {
 
     const url = decodeURIComponent(parseCookies().OBRA_THR)
     const token = parseCookies().TOKEN_OBRA;
-    console.log(token)
 
-    return<div className={style.body}>Home</div>
+    const [login, setLogin] = useState({
+        "apelido": "",
+        "senha": ""
+    })
+
+    return (
+        <div className={style.body}>
+            <div className={style.container} >
+                <div className={style.wrapContainer} >
+                    <div className={[style.card]} >
+                        <div className={style.cardLogo} >
+                            <img style={{ width: 350 }} src="../logoMarcaSemFundo.png" />
+
+                        </div>
+
+                    </div>
+                    <div className={style.card} >
+                        <div className={style.wrapContainerCard}>
+                            <div className={style.containerTitle} >
+                                <h1>
+                                    LOGIN
+                                </h1>
+
+                            </div>
+                            <div className={style.containerForm} >
+                                <div className={style.wrapInput} >
+                                <input className={login.apelido!== '' ? `${style.has_val} ${style.input}` : style.input} onChange={e => setLogin({...login,apelido: e.target.value})} />
+                                    <span className={style.focus_input} data-placeholder="Usuário... " />
+                                </div>
+                                <div className={style.wrapInput} >
+                                <input className={login.senha!== '' ? `${style.has_val} ${style.input}` : style.input} onChange={e => setLogin({...login,senha: e.target.value})} />
+                                    <span className={style.focus_input} data-placeholder="Usuário... " />
+                                </div>
+                            </div>
+                            <div className={style.containerButton} >
+                                <div className={style.wrapButton}>
+                                    <Button 
+                                    action={() => console.log(login)}
+                                    theme={"login"}
+                                    text={"ENTRAR"}
+                                    />
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    )
+
 }
 export default Home
