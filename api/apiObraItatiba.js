802bd93d8c775737e2e05678d3c03177af521a30
Vitor Hugo <vitorhugorodrigues93@gmail.com>
2023-04-24 13:26:43 -0300

    endereço api externa

--- api/apiObraItatiba.js
+++ api/apiObraItatiba.js
@@ -1,14 +1,26 @@
 import axios from "axios";
 import { parseCookies } from "nookies";
 
+const ipAddress = process.env.REACT_APP_IP_ADDRESS || "192.168.1.227";
+
+let baseURL = "";
+
+if (ipAddress.startsWith("192.168.1.227")) {
+    console.log(ipAddress)
+    baseURL = `http://${ipAddress}:9090/api`;
+} else if (ipAddress.startsWith("189.1.166.143")) {
+    console.log(ipAddress)
+    baseURL = `http://${ipAddress}:9090/api`;
+}
+
 const ApiObra = axios.create({
 
-    baseURL: "http://192.168.1.227:9090/api"
+    baseURL: baseURL
 })
 
 ApiObra.interceptors.request.use((request) => {
     const token = parseCookies("TOKEN_OBRA");
-    if(token){
+    if (token) {
         request.headers.Authorization = `Bearer ${token.TOKEN_OBRA}`;
     } return request;
 })
